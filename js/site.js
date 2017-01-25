a90a7ce63e1fe1a98b29772e7e9f1a78bae16197
Sue Jensen <suedinym@gmail.com>
2017-01-25 13:47:52 -0500

    added sliding side nav and top nav. side nav is working, top nav, not so much.

--- js/site.js
+++ js/site.js
@@ -1,11 +1,23 @@
+/* Sliding side navigation */
 /* Set the width of the side navigation to 40% */
-function openNav() {
+function openSideNav() {
     document.getElementById("mySidenav").style.width = "40%";
    
 }
 
 /* Set the width of the side navigation to 0 */
-function closeNav() {
+function closeSideNav() {
     document.getElementById("mySidenav").style.width = "0";
      
+}
+
+/* Sliding top navigation */
+/* Open */
+function openTopNav() {
+    document.getElementById("myTopNav").style.height = "40%";
+}
+
+/* Close */
+function closeTopNav() {
+    document.getElementById("myTopNav").style.height = "0%";
 }

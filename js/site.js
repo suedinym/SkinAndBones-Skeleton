19a02f01da1d2ab445eba83fec68ca508f8c0d99
Sue Jensen <suedinym@gmail.com>
2017-01-29 23:35:10 -0500

    Worked on javascript closing and opening side navigation. now there is a media query and an event listner in the javascript.

--- js/site.js
+++ js/site.js
@@ -6,12 +6,31 @@ function openTopNav() {
 function closeTopNav() {
     document.getElementById("slidingTopNav").style.height = "0%";
 }
+
+
+var slideMq = window.matchMedia("(min-width: 479px)");
+
 /* Sliding side navigation */
-function openSideNav() {
-    document.getElementById("slidingSideNav").style.width = "45%";
+ function openSideNav() {
+if (slideMq.matches) {
+  /* the viewport is at least 479 pixels wide */
+  document.getElementById("slidingSideNav").style.width = "40%";
+  
+} else {
+  /* the viewport is less than 479 pixels wide */
+  document.getElementById("slidingSideNav").style.width = "100%";
 }
+
+
+}
+slideMq.addListener(function(changed) {
+   
+       document.getElementById("slidingSideNav").style.width = "0";
+   
+});
+
+
 
 function closeSideNav() {
     document.getElementById("slidingSideNav").style.width = "0%";
 }
-
